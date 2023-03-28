0046654ef0163b7efc4fa09dd596ccdda667cdff
AliG140978 <123089540+AliG140978@users.noreply.github.com>
2023-03-28 18:51:31 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,6 @@
+console.log ("Hola mundo");
+
+/** este es mi primer proyecto, se que es basico y el diseño no es sobresaliente pero lo hice con mucho cariño y muy poco tiempo para
+ * poder cumplir con los tiempos del curso y entregarlo antes de irme de viaje. 
+ * Puedeo hacerlo mucho mejor, lo importantes es haber podido cumplir con los elementos basicos.
+ */
